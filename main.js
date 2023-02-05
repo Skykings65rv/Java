707cc7b76989fddff0b8bada6ef8f0b2b044c76c
Skykings65rv <rvanwoert@mail.valenciacollege.edu>
2023-02-05 12:30:26 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+var firstName = "Ryan";
+var lastName = " Van Woert"; 
+var age = "18"; 
+alert ('My name is ' + firstName + lastName + '!');
+alert ('I am ' + age + '!');
+console.log ('I am having so much fun in Intro to Web Programming. I am finally able to learn code and use it in a fun way!')
